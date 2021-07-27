630ed2c6bf7b54c8ff9e935afd150c7e012ca894
renaud gaudin <rgaudin@gmail.com>
2021-07-27 18:13:06 +0000

    clean logging of mediawiki init script

--- mediawiki-init.sh
+++ mediawiki-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Database : $DATABASE_NAME ($DATABASE_TYPE)"
+echo "Mediawiki initialization script for database : $DATABASE_NAME ($DATABASE_TYPE)"
 
 DATA_DIR=/var/www/data
 DATABASE_FILE=${DATA_DIR}/${DATABASE_NAME}.sqlite
@@ -17,7 +17,10 @@ else
   WGSERVER="\"$URL\";"
   WGCANONICALSERVER=$WGSERVER
 fi
-echo "Serving for: $WGSERVER / $WGCANONICALSERVER"
+
+echo "   Reachable from: $WGSERVER / $WGCANONICALSERVER"
+
+echo "> Writting Settings."
 
 { \
   echo "# Database" ; \
@@ -29,12 +32,13 @@ echo "Serving for: $WGSERVER / $WGCANONICALSERVER"
   echo "\$wgSQLiteDataDir = \"$DATA_DIR\";" ; \
   echo "\$wgServer        = $WGSERVER";
   echo "\$wgCanonicalServer= $WGSERVER";
-  # echo "\$wgShowExceptionDetails = true;";
+  echo "\$wgShowExceptionDetails = true;";
 } >> ./LocalSettings.php
 
 if [ "$DATABASE_TYPE" = "sqlite" ]
 then
   # Configure SQLite maintenance weekly
+  echo "> Adding SQLite maintenance script to weekly cron"
   { \
     echo "#!/bin/sh" ; \
     echo "cd ${WIKI_DIR}" ; \
@@ -42,9 +46,11 @@ then
   } > /etc/cron.weekly/wm_maintenance && chmod 0500 /etc/cron.weekly/wm_maintenance
 fi
 
+echo "> Ensuring proper folder structure and permissions"
 mkdir -p ${IMG_DIR} ${LOG_DIR} ${CFG_DIR}
 chown www-data:www-data ${DATA_DIR} ${IMG_DIR} ${LOG_DIR} ${CFG_DIR}
 
+echo "> Adding custom settings file (if any)"
 if [ -e ${CFG_DIR}/LocalSettings.custom.php ]
 then
   #existant config is found, then no use the default it
@@ -57,34 +63,39 @@ ln -s ${CFG_DIR}/LocalSettings.custom.php ./LocalSettings.custom.php
 
 if [ "$DATABASE_TYPE" = "mysql" ]
 then
-  echo "Use a Mysql database"
+  echo "> Setting mysql datadir to $DATA_DIR/mysql"
+
   # Configure Mysql data dir
   sed -i "/datadir/ s|/var/lib/mysql|$DATA_DIR/mysql|" /etc/mysql/mariadb.conf.d/50-server.cnf
   chmod +x $DATA_DIR
 
   if [ $MYSQL_INIT ]
   then
-    echo "Initialize a Mysql database"
+    echo "> Setting up mysql database (MYSQL_INIT is set)"
 
     if [ ! -d "$MYSQL_DATA" ]
     then
+      echo "  > Creating mysql datadir"
       #Create and init directory if no exist
       mkdir $MYSQL_DATA
       cp -a /var/lib/mysql $DATA_DIR/
     fi
 
+    echo "  > Starting MySQL"
     service mysql start
 
+    echo "  > Creating* database $DATABASE_NAME"
     echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" | mysql
 
     if [ -e "${MYSQL_IMPORT_FILE}" ]
     then
-      echo "Init MySQL database"
       # initialize mysql database from an owned file
+      echo "  > Importing import.sql into $DATABASE_NAME"
       mysql ${DATABASE_NAME} < $MYSQL_IMPORT_FILE
     fi
 
     #set privileges
+    echo "  > Creating user $DATABASE_NAME with appropriate privileges"
     echo "CREATE USER '${DATABASE_NAME}'@'localhost' IDENTIFIED BY '${DATABASE_NAME}';" | mysql -f
     echo "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_NAME}'@'localhost';" | mysql -f
     echo "FLUSH PRIVILEGES" | mysql -f
@@ -92,30 +103,36 @@ then
     if [ -e "${DATABASE_FILE}" ]
     then
       # import data from SQLite database
-      echo "Export data from SQLite database"
+      echo "  > Importing from SQLite file $DATABASE_FILE into $DATABASE_NAME"
+      echo "    > exporting to dump.sql"
       sqlite3 ${DATABASE_FILE} .dump > dump.sql
-      echo "Genereate dump for MySQL"
+      echo "    > transforming to out.sql"
       echo "SET FOREIGN_KEY_CHECKS=0;" > out.sql
       dump_for_mysql.py < dump.sql >> out.sql
       echo "SET FOREIGN_KEY_CHECKS=1;" >> out.sql
+      echo "    > importing out.sql"
       echo "Import data in MySQL database"
       mysql -f ${DATABASE_NAME} < out.sql
+      echo "    > removing dump.sql and out.sql"
       rm -rf dump.sql out.sql
     fi
 
     # remove settings to launch install script to populate a default database
+    echo "  > Launch install script to populate database (disabling localsettings)"
     mv ./LocalSettings.php ./LocalSettings.php.tmp
     php maintenance/install.php --dbuser $DATABASE_NAME --dbpass $DATABASE_NAME --dbname $DATABASE_NAME --pass MEDIAWIKI_ADMIN_PASSWORD $DATABASE_NAME Admin
     mv ./LocalSettings.php.tmp ./LocalSettings.php
 
     # skip rest of mediawiki init.
-    echo "MYSQL INIT COMPLETE. Please stop container, remove 'MYSQL_INIT=1' env and restart."
+    echo "  > End of MySQL initialization. Please stop container, remove 'MYSQL_INIT=1' env and restart."
     exit 1
   else
+    echo "  > Starting MySQL"
     service mysql start
   fi
 
   # Adding mysqldump to crontab
+  echo "> Adding SQLite maintenance script to weekly cron"
   { \
     echo "#!/bin/sh" ; \
     echo "mysqldump --databases ${DATABASE_NAME} > ${DATA_DIR}/${DATABASE_NAME}.sql" ; \
@@ -126,7 +143,7 @@ fi
 
 if [ ! -z $VOLUME_TAR_URL ]
 then
-  echo "Initialize data dir with a tar -> download it"
+  echo "> Setup data dir using tar file at $VOLUME_TAR_URL"
   curl -fSL $VOLUME_TAR_URL | tar -xz -C $DATA_DIR
   ln -s ${DATA_DIR} data
   ln -s ${DATA_DIR}/download ../download
@@ -136,38 +153,49 @@ if [ "$DATABASE_TYPE" = "sqlite" ]
 then
   if [ -e ${DATABASE_FILE} ]
   then
-    echo "SQLite Database already initialized"
+    echo "> Using existing SQLite file"
   else
-    echo "Initialize an empty SQLite database"
+    echo "> Setting up SQlite database using template one"
     #Copy the "empty" database
     cp /tmp/my_wiki.sqlite ${DATABASE_FILE}
   fi
+  # Allow to write on database
+  echo "  > Setting permissions"
+  chmod 644 ${DATABASE_FILE} && chown www-data:www-data ${DATABASE_FILE}
 fi
 
 # Allow to custom a few content at site root
 if [ -d ${DATA_SITE_ROOT_DIR} ]
 then
+    echo "> Adding static files to site_root"
     for FILE in $(find ${DATA_SITE_ROOT_DIR} -type f)
     do
+        echo "  > $(basename ${FILE})"
         FILE=$(readlink -f ${FILE})
         rm -f ../$(basename ${FILE})
         ln -s ${FILE} ../
     done
 else
+    echo "> Creating empty site_root"
     mkdir ${DATA_SITE_ROOT_DIR}
 fi
 
 # Set the secret token to download datadir
+echo "> Setting secret export token"
 sed -i s/?ACCESS_TOKEN?/$EXPORT_TOKEN/g ../export_data.php
 
 # Fix latence problem
+echo "> Removing locks"
 rm -rf ${DATA_DIR}/locks
 
-# Allow to write on database
-chmod 644 ${DATABASE_FILE} && chown www-data:www-data ${DATABASE_FILE}
-
-echo "Starting Mediawiki maintenance ..."
-maintenance/update.php --quick > ${LOG_DIR}/mw_update.log
+echo "> Running update script"
+php maintenance/update.php --quick > /tmp/update.log
+if [ $? -gt 0 ]
+then
+  echo "/!\ update script failed:"
+  cat /tmp/update.log
+fi
 
 #change Admin password
+echo "> Setting Admin user password"
 php maintenance/createAndPromote.php --bureaucrat --sysop --force Admin ${MEDIAWIKI_ADMIN_PASSWORD}
